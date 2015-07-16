6420f1ff739f0f93ec12e2ce2a4b45fe0e87d56d
bombaleyloo <bombaleyloo@gmail.com>
2015-07-16 19:51:12 +0300

    buildTable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-;(function (window){
+(function (window){
 	
 
 	var defaults = {
@@ -37,12 +37,23 @@
 	 * 
 	 * @return {DOMElement} - таблица
 	 */
-	function renderMonth () {
-		var data = [];
+	function renderMonth (date) {
+		function addDay(_day, _isCurrentMonth, _date){
+			return {day: _day, isCurrentMonth: _isCurrentMonth, date: _date};
+		}
 
-		return buildTable(data);
+		var month = date.getMonth();
+		var year = date.getFullYear();
+		date = new Date(year, month, 1);
+		var results = [];
+		while(date.getMonth() == month){
+			var day = date.getDate();
+			var dayInfo = [addDay(day, true, new Date(year, month, day))];
+			results.push(dayInfo);
+			date.setDate(date.getDate() + 1);
+		}
+		return buildTable(results);
 	}
-
 	/**
 	 * Строит таблицу по указанным данным
 	 * 
@@ -65,8 +76,48 @@
 	 * ]
 	 */
 	function buildTable (data) {
+		var days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+		var table = document.createElement('table');
+		table.appendChild(document.createElement('thead'));
+		table.lastElementChild.appendChild(document.createElement('tr'));
+		var HeadTr = table.lastElementChild.lastElementChild;
+		
+		for (var j = 0; j < 7; j++) {
+			HeadTr.appendChild(document.createElement('td'));
+			HeadTr.lastElementChild.appendChild(document.createTextNode(days[j]));
+		};
+		for (var i = 0; i < 5; i++) {
+			table.appendChild(document.createElement('tr'));
+			for (var j = 0; j < 7; j++) {
+				table.lastElementChild.appendChild(document.createElement('td'));
+			};
+		};
 
-	}
+		var initialDay = data[0][0].date.getDay();
+		var dayIndex = 0;
+		for (var j = initialDay; j <= 6; j++) {
+			table.children[1].children[j].appendChild(document.createTextNode(data[dayIndex][0].day));
+			dayIndex++;
+		};
+
+		var rowIndex = 2;
+		var tr = table.children[rowIndex];
+		while (dayIndex < data.length) {
+			var td = tr.children[0];
+			while (td && (dayIndex < data.length)) {
+				td.appendChild(document.createTextNode(data[dayIndex][0].day));
+				td = td.nextSibling;
+				dayIndex++;
+			}
+			rowIndex++;
+			tr = table.children[rowIndex];
+		}
+		return table;
+
+	};
+
+	var root = document.querySelector('.container');
+	root.appendChild(renderMonth(new Date));
 
 	window.Calendar = Calendar;
 

f0b8a22f6d7f8a1c4dca990f05f33803699e253f
saucebwz <saucebwz@yahoo.com>
2015-07-16 17:35:10 +0300

    Adding month structure for calendar rendering

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 ;(function (window){
-	
+	var d = new Date(2012, 3);
+	alert(d);
+
 	var defaults = {
 		minDate: new Date('1-1-1960'),
 		maxDate: new Date('1-1-2060')
-	}
+	};
+
+
 
 	/**
 	 * @constructor
@@ -13,7 +17,33 @@
 	 */
 	var Calendar = function (element) {
 		this.element = element;
-	}
+	};
+	/**
+	 *
+	 * @param data
+	 * @returns {Array}
+	 *
+	 */
+
+	Calendar.prototype.renderMonth = function(data){
+		function addDay(_day, _isCurrentMonth, _date){
+			return {day: _day, isCurrentMonth: _isCurrentMonth, date: _date};
+		}
+		var month = data.getMonth();
+		var year = data.getYear();
+		data = new Date(year, month, 1);
+		var results = [];
+		while(data.getMonth() == month){
+			var day = data.getDate();
+			var dayInfo = [addDay(day, true, new Date(year, month, day))];
+			results.push(dayInfo);
+			data.setDate(data.getDate() + 1);
+		}
+		return results;
+	};
+
+
+
 
 	/**
 	 * @public
@@ -24,4 +54,4 @@
 		// TODO: Объединять defaults и options
 	};
 
-})(window)
+})(window);
